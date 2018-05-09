1849b762863f9a4de619cf48127405a0da1b5bb1
DESKTOP-KQBVSJ0\charl <charlotte0388@gmail.com>
2018-05-10 00:26:37 +0800

    4, 加註解

--- BMI WPF2/MainWindow.xaml.cs
+++ BMI WPF2/MainWindow.xaml.cs
@@ -25,24 +25,12 @@ namespace BMI_WPF2
             InitializeComponent();
         }
 
-        // 把身高、體重、bmi等會用到的數值設為全域變數
-        public class Global
-        {
-           public static string height1, weight1;
-            public static float H, bmi1, W;
-        }
-
-        // 拖動體重條的觸發事件
-        //private void w_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-       // {
-            
-
-            
-       // }
-
+        // 拖動身高條及體重條的觸發事件
         private void ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            // 令Heightnumber顯示身高條的數值
             Heightnumber.Text = h.Value.ToString("0.0");
+            // 令Heightnumber跟著滑桿
             Canvas.SetLeft(height, h.Value * 2);
 
             // 令Weightnumber顯示體重條的數值
@@ -50,18 +38,28 @@ namespace BMI_WPF2
             // 令Weightnumber跟著滑桿
             Canvas.SetLeft(weight, w.Value * 4);
 
+            // 設變數H、W的值分別為Heightnumber及Weightnumber顯示的數值
             double H = double.Parse(Heightnumber.Text);
             double W = double.Parse(Weightnumber.Text);
 
+            // 計算bmi值
             double bmi = W / Math.Pow((H / 100), 2);
+
+            // 將bmi值分為整數部分及小數部分
             string[] parts = bmi.ToString("0.00").Split('.');
+
+            // 整數部分為parts[0]，顯示在bminumber1
             bminumber1.Text = parts[0];
+
+            // 判斷是否有小數
             if (parts.Length > 1)
             {
+                // 有的話將小數顯示在bminumber2
                 bminumber2.Text = "." + parts[1];
             }
             else
             {
+                // 沒有的話bminumber2顯示0
                 bminumber2.Text = ".0";
             } 
         }
